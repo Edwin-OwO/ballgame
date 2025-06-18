29a089ff531b0f3aa0fe31080e64e1107b37c322
Matias Fradin <pplus5927@gmail.com>
2025-06-18 19:38:34 -0300

    deleted speed buff

--- balls/Assets/pelota/playermovement.cs
+++ balls/Assets/pelota/playermovement.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,50 +6,12 @@ namespace TopDown.Movement
     [RequireComponent(typeof(PlayerInput))]
     public class playermovement : Mover
     {
-        private Vector3 currentInput;
-        private float originalSpeed;
-        private bool isBoosting = false;
-        private Coroutine currentBoost;
 
-        public float speed = 5f; // Velocidad base del jugador
-
-        private void Awake()
-        {
-            originalSpeed = speed;
-        }
-
-        private void Update()
-        {
-            // Movimiento tipo top-down (sin gravedad)
-            transform.position += currentInput * speed * Time.deltaTime;
-        }
 
         private void OnMove(InputValue value)
         {
-            // Captura el input (X e Y) y lo guarda
-            Vector2 input = value.Get<Vector2>();
-            currentInput = new Vector3(input.x, input.y, 0);
-        }
-
-        internal void ActivateSpeedBoost(float boostMultiplier, float boostDuration)
-        {
-            // Si ya hay un boost activo, lo reinicia
-            if (currentBoost != null)
-                StopCoroutine(currentBoost);
-
-            currentBoost = StartCoroutine(SpeedBoostCoroutine(boostMultiplier, boostDuration));
-        }
-
-        private IEnumerator SpeedBoostCoroutine(float boostMultiplier, float boostDuration)
-        {
-            isBoosting = true;
-            speed = originalSpeed * boostMultiplier;
-
-            yield return new WaitForSeconds(boostDuration);
-
-            speed = originalSpeed;
-            isBoosting = false;
-            currentBoost = null;
+            Vector3 playerInput = new Vector3(value.Get<Vector2>().x, value.Get<Vector2>().y, 0);
+            currentInput = playerInput;
         }
     }
 }
